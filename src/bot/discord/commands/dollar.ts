621e2dc7ffb33e4b4fccaaf72f925ab540573b93
Jose Salcido <jmsalcidoaguilar@gmail.com>
2023-12-13 17:42:50 -0700

    🧪 remove un-needed logging

--- src/bot/discord/commands/dollar.ts
+++ src/bot/discord/commands/dollar.ts
@@ -1,14 +1,12 @@
 import {CommandInteraction, SlashCommandBuilder} from 'discord.js';
 import {getBotMessage} from '../../../data/network';
 import {DiscordCommand} from "./types";
-import logger from "../../../logger";
 
 export const data = new SlashCommandBuilder()
   .setName('dolar')
   .setDescription('Verificar precio del dolar en diferentes bancos')
 
 export const execute = async (interaction: CommandInteraction) => {
-  logger.info("Executing command: dollar")
   const message = await getBotMessage()
   await interaction.reply(message)
 }
